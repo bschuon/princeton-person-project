a0387a824710d70da978510f23d634ac88deb5a2
Zach <zbunde@gmail.com>
2016-09-14 12:05:43 -0600

    added all surveys

---
+++ surveys/realworldsharing.js
@@ -0,0 +1,380 @@
+{
+"pages": [
+{
+"id": "0a4b9fe36e7327f359a2305eee439d2f",
+"number": 1,
+"name": null,
+"description": null,
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+},
+"elements": [
+{
+"id": "53f4a35a2aae4211c101df90c3ddca8f",
+"orderNo": 1,
+"type": "question",
+"question": {
+"id": "30e42cccd0b44c604f20e43b063cc85e",
+"text": "1.",
+"type": "grid",
+"required": true,
+"grid": {
+"rows": [
+{
+"id": "a891fa0dc989566696404cb9cb9ed6e9",
+"orderNo": 1,
+"label": "How much does your friend trust you?"
+},
+{
+"id": "c4b4e4bafddee660e09de7d547e4f33c",
+"orderNo": 2,
+"label": "How much do you trust your friend?"
+}
+],
+"cols": [
+{
+"id": "8d98f992fca05f62993797159d596368",
+"orderNo": 1,
+"label": "I don't know this person well enough to decide"
+},
+{
+"id": "be970cd1ad5ecd6c062ce58249a60601",
+"orderNo": 2,
+"label": "I don't trust this person"
+},
+{
+"id": "fef398c943890b280dff83c61f514fc2",
+"orderNo": 3,
+"label": "I trust this person somewhat"
+},
+{
+"id": "3940fe29e2670a0003b7b5acc021bc46",
+"orderNo": 4,
+"label": "I generally trust this person"
+},
+{
+"id": "c21a0e97c034dccddeb80c8315fa0ea0",
+"orderNo": 5,
+"label": "I highly trust this person"
+},
+{
+"id": "3b378d1ef7499fa9582d19ce85db04e3",
+"orderNo": 6,
+"label": "I would trust this person with my life"
+}
+]
+},
+"pageFlowModifier": false
+}
+},
+{
+"id": "c11d3d65b627c78ae5579096e12da0a1",
+"orderNo": 2,
+"type": "question",
+"question": {
+"id": "eef3336762840df5c4d413fdd052dfcd",
+"text": "2.",
+"type": "grid",
+"required": true,
+"grid": {
+"rows": [
+{
+"id": "34a232a8e3537eb308f81cbad23e58d2",
+"orderNo": 1,
+"label": "How well do you feel you know your friend?"
+},
+{
+"id": "2661da19eb6e2379a45f4dba91acba1c",
+"orderNo": 2,
+"label": "How well do you feel your friend knows you?"
+}
+],
+"cols": [
+{
+"id": "8c4a4d902cde3672600c8dcc25a143f2",
+"orderNo": 1,
+"label": "Not at all"
+},
+{
+"id": "11461d35de852ceb3fa484da46c49873",
+"orderNo": 2,
+"label": "A little bit"
+},
+{
+"id": "29fd8056967988a0301a1e9b29194cd8",
+"orderNo": 3,
+"label": "Somewhat"
+},
+{
+"id": "d1cbaa0e9b561b3ddd14e844f2041f49",
+"orderNo": 4,
+"label": "Fairly well"
+},
+{
+"id": "f527d48dee56ce2cc6b16cbece29314c",
+"orderNo": 5,
+"label": "Very well"
+},
+{
+"id": "b1c4012928866dec87df41b273e140f1",
+"orderNo": 6,
+"label": "Extremely well"
+},
+{
+"id": "7ca508f22f5d4375e03a6e63ea2df359",
+"orderNo": 7,
+"label": "Could not know any better"
+}
+]
+},
+"pageFlowModifier": false
+}
+},
+{
+"id": "b2775cfad32ba0befb641b1b06c03cd7",
+"orderNo": 3,
+"type": "question",
+"question": {
+"id": "349d779898361107de8959bd9d8865dd",
+"text": "On average, how many emails do you RECEIVE per day?",
+"type": "text",
+"required": true,
+"pageFlowModifier": false
+}
+},
+{
+"id": "daaa621a694de593a02e16b4e577362b",
+"orderNo": 4,
+"type": "question",
+"question": {
+"id": "d559309e4bab2290e7bd55039b4c1431",
+"text": "Do you have a Twitter account?",
+"type": "radio",
+"required": true,
+"offeredAnswers": [
+{
+"id": "ae3e9a061f1bd72b0ab6bbab661cad99",
+"orderNo": 1,
+"value": "Yes",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "0e991a1a2747be2e89ddb78e9e2ba53e",
+"orderNo": 2,
+"value": "No",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+}
+]
+}
+},
+{
+"id": "3009e629a7bb7a5174b4ffeb556d88c1",
+"orderNo": 5,
+"type": "question",
+"question": {
+"id": "d8895e68070320679cdd8b8bb7f00b42",
+"text": "How often do you write reviews for the things you buy (Yelp, Amazon, etc)?",
+"type": "radio",
+"required": true,
+"offeredAnswers": [
+{
+"id": "90be728ba1e936c7b3db00c97f27f4a8",
+"orderNo": 1,
+"value": "Never",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "532f3f4270f7df6df617c19c4580c74a",
+"orderNo": 2,
+"value": "On occassion",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "89c20db2f6672267991bf5287b2c4207",
+"orderNo": 3,
+"value": "About half of the time",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "71c63e15523a92bfcdd4910e64ea438e",
+"orderNo": 4,
+"value": "Most of the time",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "1ef730398deb17526f81281822845a67",
+"orderNo": 5,
+"value": "Always",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+}
+]
+}
+},
+{
+"id": "ec2b017bd8be37dd40864a15a91bdd17",
+"orderNo": 6,
+"type": "question",
+"question": {
+"id": "e4fa819f3267092c85e9dd024a971efa",
+"text": "How much time (in minutes) do you spend on email in a typical day?",
+"type": "text",
+"required": true,
+"pageFlowModifier": false
+}
+},
+{
+"id": "18a37ade7e1538458a86b3160413e74e",
+"orderNo": 7,
+"type": "question",
+"question": {
+"id": "1c34c6454b48ca50f764ad3357fce0c2",
+"text": "3.",
+"type": "grid",
+"required": true,
+"grid": {
+"rows": [
+{
+"id": "1bb3983c7a21b323326aa30c2f63ae50",
+"orderNo": 1,
+"label": "How often do you write reviews for the things you buy (Yelp, Amazon, etc)?"
+},
+{
+"id": "653d8f6eeda4b865a550cdf9b8dc4ce5",
+"orderNo": 2,
+"label": "How often do you spend time with your friends?"
+}
+],
+"cols": [
+{
+"id": "22a608c2348adba77b5d07a04820b5b2",
+"orderNo": 1,
+"label": "Never"
+},
+{
+"id": "3b2278d302e5f4341b9cbaa03a58028d",
+"orderNo": 2,
+"label": "A few times every year"
+},
+{
+"id": "34661d966217fd0f6f8a481352975e01",
+"orderNo": 3,
+"label": "A few times every year"
+},
+{
+"id": "4d961c270628481a47a008e6ee01e7be",
+"orderNo": 4,
+"label": "A few times every week"
+},
+{
+"id": "019f1360c5150535cd9a8597ca5ed147",
+"orderNo": 5,
+"label": "Every day"
+}
+]
+},
+"pageFlowModifier": false
+}
+},
+{
+"id": "8eac98a6dda9cefb8c9effd1eba1de41",
+"orderNo": 8,
+"type": "question",
+"question": {
+"id": "2bb42f9fa372d13ed5672013a9467ffd",
+"text": "Do you have Facebook?",
+"type": "radio",
+"required": true,
+"offeredAnswers": [
+{
+"id": "ad92ad36035ece194aaafd6512471cf5",
+"orderNo": 1,
+"value": "Yes",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "46c74385d584bfd3f934b553d474c57c",
+"orderNo": 2,
+"value": "No",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+}
+]
+}
+},
+{
+"id": "fd70075effc90bfc4717604894bcdce3",
+"orderNo": 9,
+"type": "question",
+"question": {
+"id": "74fb7bbebc0a631d3d62232b0131784f",
+"text": "Do you use Instagram?",
+"type": "radio",
+"required": true,
+"offeredAnswers": [
+{
+"id": "8a4c6450f9c9901ae4ad74f68d1cd474",
+"orderNo": 1,
+"value": "Yes",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+},
+{
+"id": "d735e3d5f3e8e48630d36b76c1355dff",
+"orderNo": 2,
+"value": "No",
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+}
+}
+]
+}
+},
+{
+"id": "38ea5648bc5e09ba509a33a2e470c3a1",
+"orderNo": 10,
+"type": "question",
+"question": {
+"id": "5303b412b5fa9feeb9635c90be32aaba",
+"text": "On average, how many emails do you SEND per day?",
+"type": "text",
+"required": true,
+"pageFlowModifier": false
+}
+}
+],
+"namedPage": false
+}
+],
+"name": "Real World Sharing",
+"description": "Estimated time to complete: 20 minutes"
+}
---
+++ surveys/social.js
@@ -0,0 +1,152 @@
+{
+"name": "MOS Social Support",
+"description": "Form description ",
+"pages": [
+{
+"id": "",
+"number": 1,
+"name": null,
+"description": null,
+"pageFlow": {
+"nextPage": true,
+"label": "mwForm.pageFlow.goToNextPage"
+},
+"elements": [
+{
+"id": "726eadbdd67f83b4b692c0de0b3c995c",
+"orderNo": 1,
+"type": "question",
+"question": {
+"id": "ed05240d174a3e0085d30733cdcb7354",
+"text": "Next are some questions about the support that is available to you.",
+"type": "grid",
+"required": true,
+"grid": {
+"rows": [
+{
+"id": "cc1b73e43c917447b514186e10da2f2e",
+"orderNo": 1,
+"label": "Someone to get together with for relaxation"
+},
+{
+"id": "9b591f562bae0f5386344ff5de6eb8c7",
+"orderNo": 2,
+"label": "Someone to give you information to help you understand a situation"
+},
+{
+"id": "a134175537958f26eeacf752cd3ee588",
+"orderNo": 3,
+"label": "Someone whose advice you really want"
+},
+{
+"id": "6246cf04268f000bec51f7f63869d3fe",
+"orderNo": 4,
+"label": "Someone who understands your problems"
+},
+{
+"id": "5e9ec486366a94186b9128f7d351f9c5",
+"orderNo": 5,
+"label": "Someone to take you to the doctor if you needed it"
+},
+{
+"id": "2ed4267e3473a8ee044121432571b49b",
+"orderNo": 6,
+"label": "Someone to share your most private worries and fears with"
+},
+{
+"id": "99549bd249643b7da0fba777505dd489",
+"orderNo": 7,
+"label": "Someone to confide in or talk to about yourself or your problems"
+},
+{
+"id": "a697b1d47973e0ce8c1e778201841dd0",
+"orderNo": 8,
+"label": "Someone to give you good advice about a crisis"
+},
+{
+"id": "35294f4df09c6815374947d84e79cfdf",
+"orderNo": 9,
+"label": "Someone to turn to for suggestions about how to deal with a personal problem"
+},
+{
+"id": "2b78d08e9e1be7a411ae4154fa31e2b2",
+"orderNo": 10,
+"label": "Someone you can count on to listen to you when you need to talk"
+},
+{
+"id": "8441b52bfdc041c87a0f128347e8bfa5",
+"orderNo": 11,
+"label": "Someone to do things with to help get your mind off things"
+},
+{
+"id": "b68ccecfa6d4982df6cf0ff0e12139db",
+"orderNo": 12,
+"label": "Someone who shows you love and affection"
+},
+{
+"id": "f69d6c68b81f08905e51ebfed7858490",
+"orderNo": 13,
+"label": "Someone to prepare your meals if you were unable to do it yourself"
+},
+{
+"id": "ed9c0bb52e992689e2c9d583f6d1aa94",
+"orderNo": 14,
+"label": "Someone who hugs you"
+},
+{
+"id": "d33ed3d83ae5573d35cf7f95af10d4ec",
+"orderNo": 15,
+"label": "Someone to help you if you were confined to bed"
+},
+{
+"id": "f94b6a90519cf8980ef813307b5ca256",
+"orderNo": 16,
+"label": "Someone to help with daily chores if you were sick"
+},
+{
+"id": "7305137511b2b2d04e5d0637d8628596",
+"orderNo": 17,
+"label": "Someone to have a good time with"
+},
+{
+"id": "fba78175356471f536eed20a0907ea0b",
+"orderNo": 18,
+"label": "Someone to love and make you feel wanted"
+}
+],
+"cols": [
+{
+"id": "d0c263d247b35887f344cb9019230940",
+"orderNo": 1,
+"label": "1 - None of the time"
+},
+{
+"id": "f53c87129fe1086e970b8277569f3fa4",
+"orderNo": 2,
+"label": "2 - A little of the time"
+},
+{
+"id": "e4405d1597831dcd7e0f85471f015291",
+"orderNo": 3,
+"label": "3 - Some of the time"
+},
+{
+"id": "e2ec2b9d26149870d65e9b5ed893b8f2",
+"orderNo": 4,
+"label": "4 - Most of the time"
+},
+{
+"id": "f89caa7c402c493b41047c369e3299bc",
+"orderNo": 5,
+"label": "5 - All of the time"
+}
+]
+},
+"pageFlowModifier": false
+}
+}
+],
+"namedPage": false
+}
+]
+}
