ad452fe20a8a9729f15083bf81a1c0197185ee82
Jonathan Baughn <jonathan.baughn@gmail.com>
2016-09-14 11:43:29 -0600

    more routes

--- server/routes/api/v1/surveyresponses.js
+++ server/routes/api/v1/surveyresponses.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var SurveyResponse = require('../../../models/surveyresponse');
 
 
+// submit survey response
 router.post('/', function(req, res) {
     var version_id = req.body.response.version_id;
     var user_id = req.session.passport.user || req.body.userToken;
     var recorded_time = req.body.response.recorded_time; // in seconds
-    var response = req.body.response.data;
+    var response = req.body.response.response;
     new SurveyResponse({
 	version: version_id,
 	user_id: user_id,
@@ -16,6 +17,23 @@ router.post('/', function(req, res) {
     }).save().then(function(model) {
 	res.json({valid: true});
     }).catch(function(err) {
+	console.log(err);
+	res.status(500).json({valid: false, error: err.message || err});
+    });
+});
+
+// fetch survey response
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+    SurveyResponse.fetch(id).then(res.json).catch(function(err) {
+	console.log(err);
+	res.status(500).json({valid: false, error: err.message || err});
+    });
+});
+
+// list survey responses
+router.get('/', function(req, res) {
+    SurveyResponse.fetchAll().then(res.json).catch(function(err) {
 	console.log(err);
 	res.status(500).json({valid: false, error: err.message || err});
     });
